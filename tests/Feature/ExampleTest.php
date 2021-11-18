0d62ca09627f0ac0272cefdcc8c965216017a499
Jacob Baker-Kretzmar <jacobtbk@gmail.com>
2021-11-17 22:50:39 -0500

    Add failing test

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -2,20 +2,38 @@
 
 namespace Tests\Feature;
 
+use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Facades\Schema;
 use Tests\TestCase;
 
 class ExampleTest extends TestCase
 {
-    /**
-     * A basic test example.
-     *
-     * @return void
-     */
-    public function test_example()
-    {
-        $response = $this->get('/');
+    use RefreshDatabase;
 
-        $response->assertStatus(200);
+    public function test_first_example()
+    {
+        $this->expectNotToPerformAssertions();
+
+        Schema::create('table_one', function (Blueprint $table) {
+            $table->xml('test_xml');
+        });
+
+        Schema::table('table_one', function (Blueprint $table) {
+            $table->renameColumn('test_xml', 'test_renamed');
+        });
+    }
+
+    public function test_second_example()
+    {
+        $this->expectNotToPerformAssertions();
+
+        Schema::create('table_two', function (Blueprint $table) {
+            $table->xml('test_more_xml');
+        });
+
+        Schema::table('table_two', function (Blueprint $table) {
+            $table->renameColumn('test_more_xml', 'test_renamed_again');
+        });
     }
 }
